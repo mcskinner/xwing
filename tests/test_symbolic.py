2c5c18c370a6d86a6450b7abba6c40947c9d62b2
Michael Skinner <mcskinner@users.noreply.github.com>
2022-11-04 22:42:42 -0700

    Tests to prove out Pandas use cases

--- tests/test_symbolic.py
+++ tests/test_symbolic.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from xwing import X
 
 
@@ -13,3 +15,19 @@ def test_wing():
 
     obj = Obj()
     assert X._.plus1(3)(obj) == 4
+
+
+def test_df():
+    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    expected = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [5, 7, 9]})
+
+    actual = df.assign(c=X.a + X.b)
+    pd.testing.assert_frame_equal(actual, expected)
+
+
+def test_df_nullable():
+    df = pd.DataFrame({"nullable": [1, 2, None]})
+    expected = pd.DataFrame({"nullable": [1, 2, None], "isnull": [False, False, True]})
+
+    actual = df.assign(isnull=X._.isnull())
+    pd.testing.assert_frame_equal(actual, expected)
